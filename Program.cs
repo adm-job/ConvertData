670cbb3102f57d01a2f364a9e7d026859ea62a15
Stanislav <adm.job@yandex.ru>
2025-04-10 00:51:18 +0500

    second commit

--- Program.cs
+++ Program.cs
@@ -6,17 +6,36 @@ class Program
 {
     static void Main(string[] args)
     {
-    /* ERROR Convert
-    int first = 2;
-    string second = "4";
-    int result = first + second; <----- int + string != int
-    Console.WriteLine(result);
-    */
-    
-    int first = 2;
-    string second = "4";
-    string result = first + second; // <----- int + string == string
-    Console.WriteLine(result);
-    
+        /* ERROR Convert
+        int first = 2;
+        string second = "4";
+        int result = first + second; <----- int + string != int
+        Console.WriteLine(result);
+        */
+
+        int first = 2;
+        string second = "4";
+        string result = first + second; // <----- int + string == string
+        Console.WriteLine(result);
+
+        int myInt = 3;
+        Console.WriteLine($"int: {myInt}");
+        // the expanding transformation
+        decimal myDecimal = myInt;
+        Console.WriteLine($"decimal: {myDecimal}");
+
+        myDecimal = 3.14m;
+        Console.WriteLine($"decimal: {myDecimal}");
+        // the narrowing transformation
+        myInt = (int)myDecimal; // explicit conversion
+        Console.WriteLine($"int: {myInt}");
+
+        myDecimal = 1.23456789m;
+        float myFloat = (float)myDecimal;
+
+        Console.WriteLine($"Decimal: {myDecimal}");
+        Console.WriteLine($"Float  : {myFloat}");
+
+
     }
 }
