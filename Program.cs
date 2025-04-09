dd1237261ba6985a3408f45e72f42293951c0d1c
Stanislav <adm.job@yandex.ru>
2025-04-09 23:48:38 +0500

    first commit

--- Program.cs
+++ Program.cs
@@ -6,6 +6,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+    /* ERROR Convert
+    int first = 2;
+    string second = "4";
+    int result = first + second; <----- int + string != int
+    Console.WriteLine(result);
+    */
+    
+    int first = 2;
+    string second = "4";
+    string result = first + second; // <----- int + string == string
+    Console.WriteLine(result);
+    
     }
 }
