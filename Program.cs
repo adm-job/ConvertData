949cf3f00a61e5cb3985dfc03b275b7e42183eef
Stas <adm.job@yandex.ru>
2025-04-10 22:36:24 +0500

    update

--- Program.cs
+++ Program.cs
@@ -155,13 +155,13 @@ class Program
         // decimal value2 = 6.2m;
         // float value3 = 4.3f;
 
-        // decimal result1 = value1 / value2;
-        // Console.WriteLine($"Divide value1 by value2, display the result as an int: {Convert.ToInt32(result1)}");
+        // decimal result1 = Convert.ToInt32(value1 / value2);
+        // Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
 
         // decimal result2 = value2 / (decimal)value3;
         // Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
 
-        // float result3 = value3 / (float)value1;
+        // float result3 = value3 / value1;
         // Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
     }
 }
